2d200668dedb8d0ea61676dc70e5ae79615c1576
Benjamin Kagia <kagia@users.noreply.github.com>
2015-09-28 14:55:09 +0300

    switch to using laravels elixer
    [Delivers #103166900]
    
    this uses laravel elixer to compile assets.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,45 +3,81 @@
 <head>
     <title>Resly</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="/css/normalize.css">
-    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
-    <!-- <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/css/material-fullpalette.min.css"> -->
-    <link rel="stylesheet" type="text/css" href="/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" type="text/css" href="/css/style.css">
+    <link rel="stylesheet" type="text/css" href="{!! asset('css/app.css') !!}">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/paper/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="{!! asset('css/welcome.css') !!}">
 </head>
 <body>
 
-    <div id="page" class="container-fluid">
-
-        <nav class="navbar navbar-default">
-          <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="/">Resly</a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-              <ul class="nav navbar-nav navbar-right">
-                <li><a href="/diner">Diner</a></li>
-                <li><a href="/restau">Restauranteur</a></li>
-              </ul>
-            </div><!-- /.navbar-collapse -->
-          </div><!-- /.container-fluid -->
-        </nav>
-
-        <div id="banner">
-            <h1 class="site-title">Resly</h1>
+    <div class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
         </div>
+        <div class="navbar-collapse collapse" id="navbar-main">
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="/diner" target="_blank">Diner</a></li>
+            <li><a href="/restaur" target="_blank">Restauranteur</a></li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
+
+
+    <div class="container">
+
+      <div id="banner">
+        <div class="inner">
+            <h1>Welcome to resly <small>the new way to dine</small></h1>
+        </div>
+      </div>
+    </div>
+
+    <div id="content">
+            <div class="container">
+                <div class="row">
+                  <div class="col-xs-6 col-md-3">
+                    <div class="cell">
+                        <a href="#" class="thumbnail">
+                          <img src="/img/search.jpg" alt="...">
+                        </a>
+                    </div>
+                    
+                  </div>
+                  <div class="col-xs-6 col-md-3">
+                    <div class="cell">
+                        <a href="#" class="thumbnail">
+                          <img src="/img/reservation.jpg" alt="...">
+                        </a>
+                    </div>
+                    
+                  </div>
+                  <div class="col-xs-6 col-md-3">
+                    <div class="cell">
+                       <a href="#" class="thumbnail">
+                         <img src="/img/menu.jpg" alt="...">
+                       </a> 
+                    </div>
+                    
+                  </div>
+                  <div class="col-xs-6 col-md-3">
+                    <div class="cell">
+                        <a href="#" class="thumbnail">
+                          <img src="/img/wine.jpg" alt="...">
+                        </a>
+                    </div>
+                    
+                  </div>
+                </div>
+            </div>
     </div>
 
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/js/material.min.js"></script>
 </body>
 </html>
