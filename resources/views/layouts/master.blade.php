7a1b2efdf6e5422f00036697c4983b78309e2985
Benjamin Kagia <kagia@users.noreply.github.com>
2015-09-28 16:28:24 +0300

    add master layout
    [Finishes #104316808]
    
    this adds a new layout to the project. please
    refer to the project wiki on how to use layouts.

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Resly @yield('title')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="{!! asset('css/app.css') !!}">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/paper/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="{!! asset('css/welcome.css') !!}">
+</head>
+<body>
+
+    <div class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div class="navbar-collapse collapse" id="navbar-main">
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="/diner" target="_blank">Diner</a></li>
+            <li><a href="/restaur" target="_blank">Restauranteur</a></li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
+
+
+    @yield('content')
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,35 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Resly</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="{!! asset('css/app.css') !!}">
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/paper/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="{!! asset('css/welcome.css') !!}">
-</head>
-<body>
+@extends('layouts.master')
 
-    <div class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-        </div>
-        <div class="navbar-collapse collapse" id="navbar-main">
-
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="/diner" target="_blank">Diner</a></li>
-            <li><a href="/restaur" target="_blank">Restauranteur</a></li>
-          </ul>
-
-        </div>
-      </div>
-    </div>
+@section('title', 'welcome')
 
 
+@section('content')
     <div class="container">
 
       <div id="banner">
@@ -77,7 +51,4 @@
                 </div>
             </div>
     </div>
-
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-</body>
-</html>
+@endsection
