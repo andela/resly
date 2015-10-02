c9b2447d55d7e46564365acddcccec9067fa2266
Andela Fellow <alekikangethe@gmail.com>
2015-10-02 17:07:09 +0300

    Fix Search UI [Finishes #104704290] The changes were made so as to improve the search ui hence making search more  appealing

--- resources/views/search/results.blade.php
+++ resources/views/search/results.blade.php
@@ -1,30 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<html>
+<head>
+    <title>Resly | Results</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
-
-    <title>Resly | A one stop shop for your reservation needs</title>
-
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-
-    <!-- Custom styles for this template -->
+    <link rel="stylesheet" type="text/css" href="{!! asset('css/app.css') !!}">
     <link href="{{ asset('css/navbar-fixed-top.css') }}" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-
-  <body>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/paper/bootstrap.min.css">
+</head>
+<body>
 
     <!-- Fixed navbar -->
     <nav class="navbar navbar-default navbar-fixed-top">
@@ -36,7 +19,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Resly</a>
+          <a class="navbar-brand" href="/">Resly</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
@@ -61,7 +44,7 @@
       </div>
     </nav>
 
-    <div class="container">
+    <div class="container white">
       <h3>Results</h3>
       @if (! $results->count())
         <p>No results found, sorry</p>
@@ -74,8 +57,6 @@
       @endif
     </div>
 
-  
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  </body>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
 </html>
