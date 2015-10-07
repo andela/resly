ddf1fbc9ce2f1760b339d70a6c15125bbe0de9e6
andela-sachungo <sachungo1@gmail.com>
2015-10-07 13:52:21 +0300

    Transfer navbar section code from master to welcome files
        [fixes #105087144]
        Used section and yield

--- resources/views/diner/home.blade.php
+++ resources/views/diner/home.blade.php
@@ -52,7 +52,7 @@
           <ul class="nav navbar-nav navbar-right">
             @if (Auth::diner()->check())
             <li>Welcome {{ Auth::diner()->get()->fname }}</li> 
--           <li><a href="{{ route('dinersignout')}}">Signout</a></li>
+           <li><a href="{{ route('dinersignout')}}">Signout</a></li>
             @else
             <li><a href="{{ route('dinersignin')}}">Login</a></li>
             <li><a href="{{ route('dinersignup')}}">Register</a></li>
