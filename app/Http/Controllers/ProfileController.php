75e1cf4ccf5aff6e046a88648bd4194c222032a9
Andela Fellow <alekikangethe@gmail.com>
2015-10-13 12:45:02 +0300

    Fixed styleci errors

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,6 +9,7 @@ class ProfileController extends Controller
     public function getProfile($id)
     {
         $rest = Restaurant::where('id', $id)->first();
+
         return view('profile.index')
         ->with('rest', $rest);
     }
