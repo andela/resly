55d8764d999d86f132d44632bfa626683ce47d3a
Andela Fellow <alekikangethe@gmail.com>
2015-11-10 10:56:34 +0300

    Fix resto homepage [Finishes #107790404] The resto hompage has been redesigned to enhance UI

--- resources/views/rest/home.blade.php
+++ resources/views/rest/home.blade.php
@@ -8,9 +8,8 @@
 <meta name="description" content="">
 <meta name="author" content="">
 <link rel="icon" href="../../favicon.ico">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-<link rel="stylesheet" type="text/css" href="{!! asset('css/navbar-fixed-top.css') !!}">
-<link rel="stylesheet" type="text/css" href="{!! asset('css/auth.css') !!}">
+<link rel="stylesheet" type="text/css" href="{!! asset('css/welcome.css') !!}">
+<link rel="stylesheet" type="text/css" href="{!! asset('font-awesome/css/font-awesome.min.css') !!}">
 @endsection
 
 @section('navbar')
@@ -46,88 +45,43 @@
 @endsection
 
 @section('content')
-@if (! Auth::restaurateur()->check()) 
-   <div class="container">
-      
-    <!-- Nav tabs -->
-    <ul class="nav nav-tabs" role="tablist">
-      <li class="active">
-          <a href="#Register" role="tab" data-toggle="tab">Register</a>
-      </li>
-      <li>
-        <a href="#Login" role="tab" data-toggle="tab">Login</a>
-      </li>
-    </ul>
-    
-    <!-- Tab panes -->
-    <div class="tab-content">
-      <div class="tab-pane fade active in" id="Register">
-          
-        <div class="container">
-          <div class="col-lg-6 white">
-              <h3>Register</h3>
-              <form class="form-vertical" role="form" method="post" action="{{ route('restsignup') }}">
-                  <div class="form-group{{ $errors->has('fname') ? ' has-error' : '' }}">
-                      <label for="fname" class="control-label">First Name</label>
-                      <input type="text" name="fname" class="form-control" id="lname" value="{{ Request::old('fname') ?: '' }}">
-                      @if ($errors->has('fname'))
-                          <span class="help-block">{{ $errors->first('fname') }}</span>
-                      @endif
-                  </div>
-                  <div class="form-group{{ $errors->has('lname') ? ' has-error' : '' }}">
-                      <label for="lname" class="control-label">Last Name</label>
-                      <input type="text" name="lname" class="form-control" id="lname" value="{{ Request::old('lname') ?: '' }}">
-                      @if ($errors->has('lname'))
-                          <span class="help-block">{{ $errors->first('lname') }}</span>
-                      @endif
-                  </div>
-                  <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                      <label for="email" class="control-label">Email</label>
-                      <input type="text" name="email" class="form-control" id="email" value="{{ Request::old('email') ?: '' }}">
-                      @if ($errors->has('email'))
-                          <span class="help-block">{{ $errors->first('email') }}</span>
-                      @endif
-                  </div>
-                  <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                      <label for="password" class="control-label">Password</label>
-                      <input type="password" name="password" class="form-control" id="password">
-                      @if ($errors->has('password'))
-                          <span class="help-block">{{ $errors->first('password') }}</span>
-                      @endif
-                  </div>
-                  <div class="form-group{{ $errors->has('confirm-password') ? ' has-error' : '' }}">
-                      <label for="password" class="control-label">Confirm Password</label>
-                      <input type="password" name="confirm-password" class="form-control" id="password">
-                      @if ($errors->has('confirm-password'))
-                          <span class="help-block">{{ $errors->first('confirm-password') }}</span>
-                      @endif
-                  </div>
-                  <div class="form-group">
-                      <button type="submit" class="btn btn-primary">Sign up</button>
-                  </div>
-                  <input type="hidden" name="_token" value="{{ Session::token() }}">
-              </form>
-              <div class="or">
-                  <h2><span>OR</span></h2>
-                  <div class="social">
-                      <ul>
-                          <li><a href="{{ url('auth/facebook') }}"><i class="fa fa-facebook"></i></a></li>
-                          <li><a href="{{ url('auth/twitter') }}"><i class="fa fa-twitter"></i></a></li>
-                          <li><a href="{{ url('auth/google') }}"><i class="fa fa-google-plus"></i> </a></li>
-                      </ul>
-                  </div>
-              </div>
-          </div>
-      </div>
-    </div>
-      <div class="tab-pane fade" id="Login">
-          <div class="container">
-            <div class="col-lg-6 white">
-              <h3>Login</h3>
-                <form class="form-vertical" role="form" method="post" action="{{ route('restsignin')}}">
+  @if (! Auth::restaurateur()->check()) 
+    <div class="container">
+      <div class="rest-home">
+        <!-- Nav tabs -->
+        <ul class="nav nav-tabs" role="tablist">
+          <li class="active">
+              <a href="#Register" role="tab" data-toggle="tab">Register</a>
+          </li>
+          <li>
+            <a href="#Login" role="tab" data-toggle="tab">Login</a>
+          </li>
+        </ul>
+        <!-- Tab panes -->
+        <div class="tab-content">
+          <div class="tab-pane fade active in" id="Register">
+              
+            <div class="center">
+              <div class="col-lg-6">
+                <h3>Register</h3>
+                <form class="form-vertical" role="form" method="post" action="{{ route('restsignup') }}">
+                    <div class="form-group{{ $errors->has('fname') ? ' has-error' : '' }}">
+                        <label for="fname" class="control-label">First Name</label>
+                        <input type="text" name="fname" class="form-control" id="lname" value="{{ Request::old('fname') ?: '' }}">
+                        @if ($errors->has('fname'))
+                            <span class="help-block">{{ $errors->first('fname') }}</span>
+                        @endif
+                    </div>
+                    <div class="form-group{{ $errors->has('lname') ? ' has-error' : '' }}">
+                        <label for="lname" class="control-label">Last Name</label>
+                        <input type="text" name="lname" class="form-control" id="lname" value="{{ Request::old('lname') ?: '' }}">
+                        @if ($errors->has('lname'))
+                            <span class="help-block">{{ $errors->first('lname') }}</span>
+                        @endif
+                    </div>
                     <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                         <label for="email" class="control-label">Email</label>
-                        <input type="text" name="email" class="form-control" id="email">
+                        <input type="text" name="email" class="form-control" id="email" value="{{ Request::old('email') ?: '' }}">
                         @if ($errors->has('email'))
                             <span class="help-block">{{ $errors->first('email') }}</span>
                         @endif
@@ -139,32 +93,75 @@
                             <span class="help-block">{{ $errors->first('password') }}</span>
                         @endif
                     </div>
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="remember"> Remember me
-                        </label>
+                    <div class="form-group{{ $errors->has('confirm-password') ? ' has-error' : '' }}">
+                        <label for="password" class="control-label">Confirm Password</label>
+                        <input type="password" name="confirm-password" class="form-control" id="password">
+                        @if ($errors->has('confirm-password'))
+                            <span class="help-block">{{ $errors->first('confirm-password') }}</span>
+                        @endif
                     </div>
                     <div class="form-group">
-                        <button type="submit" class="btn btn-primary">Sign in</button>
+                        <button type="submit" class="btn btn-primary">Sign up</button>
                     </div>
                     <input type="hidden" name="_token" value="{{ Session::token() }}">
                 </form>
-                  <div class="or">
-                      <h2><span>OR</span></h2>
-                      <div class="social">
-                          <ul>
-                              <li><a href="{{ url('auth/facebook') }}"><i class="fa fa-facebook"></i></a></li>
-                              <li><a href="{{ url('auth/twitter') }}"><i class="fa fa-twitter"></i></a></li>
-                              <li><a href="{{ url('auth/google') }}"><i class="fa fa-google-plus"></i> </a></li>
-                          </ul>
-                      </div>
-                  </div>
+                <div class="or">
+                    <h2><span>OR</span></h2>
+                    <div class="social">
+                        <ul>
+                            <li><a href="{{ url('auth/facebook') }}"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="{{ url('auth/twitter') }}"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="{{ url('auth/google') }}"><i class="fa fa-google-plus"></i> </a></li>
+                        </ul>
+                    </div>
+                </div>
+              </div>
             </div>
+          </div>
+          <div class="tab-pane fade" id="Login">
+              <div class="center">
+                <div class="col-lg-6 white">
+                  <h3>Login</h3>
+                    <form class="form-vertical" role="form" method="post" action="{{ route('restsignin')}}">
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="control-label">Email</label>
+                            <input type="text" name="email" class="form-control" id="email">
+                            @if ($errors->has('email'))
+                                <span class="help-block">{{ $errors->first('email') }}</span>
+                            @endif
+                        </div>
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="control-label">Password</label>
+                            <input type="password" name="password" class="form-control" id="password">
+                            @if ($errors->has('password'))
+                                <span class="help-block">{{ $errors->first('password') }}</span>
+                            @endif
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox" name="remember"> Remember me
+                            </label>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Sign in</button>
+                        </div>
+                        <input type="hidden" name="_token" value="{{ Session::token() }}">
+                    </form>
+                      <div class="or">
+                          <h2><span>OR</span></h2>
+                          <div class="social">
+                              <ul>
+                                  <li><a href="{{ url('auth/facebook') }}"><i class="fa fa-facebook"></i></a></li>
+                                  <li><a href="{{ url('auth/twitter') }}"><i class="fa fa-twitter"></i></a></li>
+                                  <li><a href="{{ url('auth/google') }}"><i class="fa fa-google-plus"></i> </a></li>
+                              </ul>
+                          </div>
+                      </div>
+                </div>
+              </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-@else
-<p>Rest</p>
-@endif
+  @endif
 @endsection
