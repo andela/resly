1d48f909606d81749d6b1e2e9f1997fd226a5a52
andela.bmosigisi <brianmosigisi@yahoo.com>
2015-11-18 12:30:24 +0300

    remove extra padding In menu, table and restaurant add and edit pages. [finishes #108420354]

--- resources/views/restaurant/add.blade.php
+++ resources/views/restaurant/add.blade.php
@@ -19,89 +19,91 @@
     padding: 20px 20px 20px 20px;
   }
 </style>
-  <div class="col-sm-6 dark">
-    <h4>Add the details of the restaurant</h4>
-        
-    <form  method="POST" action="/restaurants/add">
-    
-      <div class="row">
-        @if (count($errors) > 0)
-        <div class="alert alert-danger">
-          <ul>
-              @foreach ($errors->all() as $error)
-              <li>{{ $error }}</li>
-              @endforeach
-          </ul>
+  <div class="content">
+    <div class="col-sm-6 dark">
+      <h4>Add the details of the restaurant</h4>
+          
+      <form  method="POST" action="/restaurants/add">
+      
+        <div class="row">
+          @if (count($errors) > 0)
+          <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+          </div>
+          @endif
         </div>
-        @endif
-      </div>
 
-      {{ csrf_field() }}
+        {{ csrf_field() }}
 
-      <!-- Add Name of restaurant -->
-      <div class= "form-group">
-        <label for= "name" class= "control-label"> Name of restaurant</label>
-        <input type = "text" class = "form-control" id= "restaurant_name" 
-            placeholder = "Enter name" name= "name" value="{{ old('name') }}">
-      </div>
+        <!-- Add Name of restaurant -->
+        <div class= "form-group">
+          <label for= "name" class= "control-label"> Name of restaurant</label>
+          <input type = "text" class = "form-control" id= "restaurant_name" 
+              placeholder = "Enter name" name= "name" value="{{ old('name') }}">
+        </div>
 
-      <!-- Add Description of restaurant -->
-      <div class ="form-group">
-        <label for= "cuisine" class= "control-label"> Description</label>
-        <textarea class = "form-control" name= "description" id = "cuisine" rows= "5" placeholder = "e.g. cuisine and selling point">{{ old('description') }}</textarea>
-      </div>
+        <!-- Add Description of restaurant -->
+        <div class ="form-group">
+          <label for= "cuisine" class= "control-label"> Description</label>
+          <textarea class = "form-control" name= "description" id = "cuisine" rows= "5" placeholder = "e.g. cuisine and selling point">{{ old('description') }}</textarea>
+        </div>
 
-      <!-- Add Opening and Closing Hours of restaurant -->
-      <div class = "form-group">
-        <div class = "row">
-          <div class = "col-sm-6">
-            <label for = "opening-hours" class = "control-label"> Opening Time</label>
-            <input class = "form-control" id = "opening-hours" name="opening_time" placeholder = "09:00 (In 24hr system)" value="{{ old('opening_time') }}">
+        <!-- Add Opening and Closing Hours of restaurant -->
+        <div class = "form-group">
+          <div class = "row">
+            <div class = "col-sm-6">
+              <label for = "opening-hours" class = "control-label"> Opening Time</label>
+              <input class = "form-control" id = "opening-hours" name="opening_time" placeholder = "09:00 (In 24hr system)" value="{{ old('opening_time') }}">
+            </div>
+            
+            <div class = "col-sm-6">
+              <label for = "closing-time" class = "control-label"> Closing Time</label>
+              <input class = "form-control" id = "closing-time" name="closing_time" placeholder = "22:00" value="{{ old('closing_time') }}">
+            </div>
+          </div>
+        </div>
+
+          <!-- Add where restaurant is located -->
+        <div class= "form-group">
+          <label for = "location" class= "control-label"> Location</label>
+          <input type = "text" class = "form-control" id= "location" name="location" placeholder = "Enter physical location" value="{{ old('location') }}">
+        </div>
+
+        <!-- Add contact information of restaurant -->
+        <fieldset>
+          <legend>Contact information</legend>
+         
+          <!-- Telephone number -->
+          <div class = "form-group">
+            <label for = "telephone" class = "control-label"> Telephone Number</label>
+            <input type = "tel" class = "form-control" id = "telephone" 
+              name="telephone" placeholder = "Enter phone number" value="{{ old('telephone') }}">
+          </div>
+
+          <!-- Email address -->
+          <div class = "form-group">
+            <label for = "email-address" class = "control-label"> Email address</label>
+            <input type = "email" class = "form-control" name="email" id = "email-address" 
+              value="{{ old('email') }}" placeholder = "Enter email address">
           </div>
           
-          <div class = "col-sm-6">
-            <label for = "closing-time" class = "control-label"> Closing Time</label>
-            <input class = "form-control" id = "closing-time" name="closing_time" placeholder = "22:00" value="{{ old('closing_time') }}">
-          </div>
-        </div>
-      </div>
+           <!-- Postal address -->
+          <div class = "form-group">
+            <label for = "postal" class = "control-label"> Postal address</label>
+            <input type = "text" class = "form-control" name="address" id = "postal" 
+              value="{{ old('address') }}" placeholder = "Enter postal address">
+          </div>  
+        </fieldset>
 
-        <!-- Add where restaurant is located -->
-      <div class= "form-group">
-        <label for = "location" class= "control-label"> Location</label>
-        <input type = "text" class = "form-control" id= "location" name="location" placeholder = "Enter physical location" value="{{ old('location') }}">
-      </div>
-
-      <!-- Add contact information of restaurant -->
-      <fieldset>
-        <legend>Contact information</legend>
-       
-        <!-- Telephone number -->
         <div class = "form-group">
-          <label for = "telephone" class = "control-label"> Telephone Number</label>
-          <input type = "tel" class = "form-control" id = "telephone" 
-            name="telephone" placeholder = "Enter phone number" value="{{ old('telephone') }}">
-        </div>
-
-        <!-- Email address -->
-        <div class = "form-group">
-          <label for = "email-address" class = "control-label"> Email address</label>
-          <input type = "email" class = "form-control" name="email" id = "email-address" 
-            value="{{ old('email') }}" placeholder = "Enter email address">
-        </div>
-        
-         <!-- Postal address -->
-        <div class = "form-group">
-          <label for = "postal" class = "control-label"> Postal address</label>
-          <input type = "text" class = "form-control" name="address" id = "postal" 
-            value="{{ old('address') }}" placeholder = "Enter postal address">
+          <button type = "submit" class = "btn btn-primary btn-lg" style="float:right">Next</button>
         </div>  
-      </fieldset>
-
-      <div class = "form-group">
-        <button type = "submit" class = "btn btn-primary btn-lg" style="float:right">Next</button>
-      </div>  
-    </form>
+      </form>
+    </div>
   </div>
 
 @endsection
