29a27026558c02cb0c7c7f12b7f653c4d252c950
alecthepadawan <alekikangethe@gmail.com>
2016-01-06 10:57:27 +0300

    (fix)Added social login flash,fixed navbar&profile [Fixes #111033396 #111034742 #110955822]

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -23,6 +23,7 @@
                   <li><a href="#">Add Wine</a></li>
                   <li><a href="{{ route('userProfile', ['username' => auth()->user()->username]) }}">Your Profile</a></li>
                   <li><a href="{{ route('userProfileEdit') }}">Edit Your Profile</a></li>
+                  <li><a href="#">Gallery</a></li>
                 @endcan
                 @can('diner-link')
                   <h3><small>Your Name</small></h3>
