d87c69a83f1215389c3f17d43b15427b33a50b2f
Adebiyi Bodunde <bodunde.adebiyi@andela.com>
2016-01-25 07:07:50 -0500

    Applied fixes from StyleCI

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -12,7 +12,6 @@ class RestaurantController extends Controller
 {
     public function __construct()
     {
-        
     }
 
     /**
@@ -21,6 +20,7 @@ class RestaurantController extends Controller
     public function getAdd()
     {
         $this->authorize('restaurateur-user');
+
         return view('restaurant.add');
     }
 
@@ -124,8 +124,7 @@ class RestaurantController extends Controller
     }
 
     /**
-     * Fetch Nearby restaurants
-     *
+     * Fetch Nearby restaurants.
      */
     public function postCloseby(Request $request)
     {
@@ -142,17 +141,17 @@ class RestaurantController extends Controller
         ->get();
 
         //prepare results in json and push
-        if( count($restaurants) > 0 ) {
+        if (count($restaurants) > 0) {
             $output = $this->constructAssoc($restaurants, $lat, $lng);
+
             return response(json_encode($output))->header('Content-Type', 'application/json');
         } else {
-            return "nothing found";
+            return 'nothing found';
         }
     }
 
-
     /**
-     * conver model results into assoc array
+     * conver model results into assoc array.
      */
     private function constructAssoc($records, $lat, $lng)
     {
@@ -163,53 +162,57 @@ class RestaurantController extends Controller
             $output[$i]['name'] = $record->name;
             $output[$i]['description'] = $record->description;
             $output[$i]['location'] = $record->location;
-            $output[$i]['distance'] = $this->calcDistance($lat, $lng, 
+            $output[$i]['distance'] = $this->calcDistance($lat, $lng,
                 $record->latitude, $record->longitude, 'km', 3);
             $i++;
         }
+
         return $output;
     }
 
     private function calcDistance(
-        $point1_lat, $point1_long, 
-        $point2_lat, $point2_long, 
-        $unit = 'km', $decimals = 2) {
+        $point1_lat, $point1_long,
+        $point2_lat, $point2_long,
+        $unit = 'km', $decimals = 2)
+    {
 
         // Calculate the distance in degrees using Hervasine formula
-        $degrees = rad2deg(acos((sin(deg2rad($point1_lat))*
-            sin(deg2rad($point2_lat))) + 
-        (cos(deg2rad($point1_lat))*
-            cos(deg2rad($point2_lat))*
-            cos(deg2rad($point1_long-$point2_long)))));
+        $degrees = rad2deg(acos((sin(deg2rad($point1_lat)) *
+            sin(deg2rad($point2_lat))) +
+        (cos(deg2rad($point1_lat)) *
+            cos(deg2rad($point2_lat)) *
+            cos(deg2rad($point1_long - $point2_long)))));
 
         // Convert the distance in degrees to the chosen unit (kilometres, miles or nautical miles)
 
-        switch($unit) {
+        switch ($unit) {
             case 'km':
-                // 1 degree = 111.13384 km, based on the average diameter of the Earth (12,735 km)    
-                $distance = $degrees * 111.13384; 
+                // 1 degree = 111.13384 km, based on the average diameter of the Earth (12,735 km)
+                $distance = $degrees * 111.13384;
                 break;
             case 'mi':
                 // 1 degree = 69.05482 miles, based on the average diameter of the Earth (7,913.1 miles)
-                $distance = $degrees * 69.05482; 
+                $distance = $degrees * 69.05482;
                 break;
             case 'nmi':
                 // 1 degree = 59.97662 nautic miles, based on the average diameter of the Earth (6,876.3 nautical miles)
-                $distance =  $degrees * 59.97662; 
+                $distance = $degrees * 59.97662;
         }
 
         return round($distance, $decimals);
     }
+
     /**
-     * fetch coordinates
+     * fetch coordinates.
      */
     private function fetchCoordinates($location)
     {
-        $client = new \GuzzleHttp\Client(['base_uri'=> 'https://maps.googleapis.com/maps/api/geocode/']);
-        $getQueryStr = 'json?address=' . urlencode($location) . '&key=' . env('GOOGLE_API_KEY') ;
+        $client = new \GuzzleHttp\Client(['base_uri' => 'https://maps.googleapis.com/maps/api/geocode/']);
+        $getQueryStr = 'json?address='.urlencode($location).'&key='.env('GOOGLE_API_KEY');
         $response = $client->get($getQueryStr);
 
         $results = json_decode($response->getBody(), true, 512);
+
         return $results['results'][0]['geometry']['location'];
     }
 }
