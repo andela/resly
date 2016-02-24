8bee147a2ad3afdaedc2dfc1637f47b27c6e7f26
andela-gjames <george.james@andela.com>
2016-02-24 08:18:06 +0000

    [Feature #114380111] Home-page fully redesigned

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Resly &#187; @yield('title')</title>
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/paper/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     {{-- <link rel="stylesheet" type="text/css" href="{!! asset('css/welcome.css') !!}"> --}}
     <link href='https://fonts.googleapis.com/css?family=Montez|Montserrat|Raleway+Dots|Raleway:100,200,300,400,700' rel='stylesheet' type='text/css'>
@@ -12,28 +12,25 @@
     @yield('styles')
 </head>
 <body>
+    {{-- <div class="container-wrapper"> --}}
+        @include('partials.navbar')
 
-    @include('partials.navbar')
-
-    @yield('content')
-
+        @yield('content')
+    {{-- </div> --}}
     {{-- <footer>
-      <div class="footer">
         <div class="container">
-          <div class="footer-inner">
-            <div class="row">
-              <div class="col-md-6 col-md-offset-3">
-                <div class="footer-content text-center padding-ver-clear">
-                  <p><a target="_blank" href="/">Resly</a> | Home for your reservation needs.</p>
-                  <div class="separator"></div>
-                  <p class="text-center margin-clear">Copyright © 2015. All Rights Reserved</p>
+            <div class="footer-inner">
+                <div class="row">
+                    <div class="col-md-6 col-md-offset-3">
+                        <div class="footer-content text-center padding-ver-clear">
+                            <p><a target="_blank" href="/">Resly</a> | Home for your reservation needs.</p>
+                            <div class="separator"></div>
+                            <p class="text-center margin-clear">Copyright © 2015. All Rights Reserved</p>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
-      <!-- .footer end -->
     </footer> --}}
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,41 +1,139 @@
-@extends('layouts.master')
+{{-- @extends('layouts.master') --}}
 
 @section('title', 'welcome')
 
 @section('styles')
   <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato:400,100' rel='stylesheet' type='text/css'>
-  {{-- <link rel="stylesheet" type="text/css" href="{!! asset('css/welcome.css') !!}">
+  {{-- <link rel="stylesheet" type="text/css" href="{!! asset('css/welcome.css') !!}"> --}}
   <link rel="stylesheet" type="text/css" href="{!! asset('font-awesome/css/font-awesome.min.css') !!}">
   <link rel="stylesheet" type="text/css" href="{!! asset('owl-carousel/owl.carousel.css') !!}">
   <link rel="stylesheet" type="text/css" href="{!! asset('owl-carousel/owl.theme.css') !!}">
   <link rel="stylesheet" type="text/css" href="{!! asset('owl-carousel/owl.transitions.css') !!}">
-  <link rel="stylesheet" type="text/css" href="{!! asset('css/rest-closeby.css') !!}"> --}}
+  {{-- <link rel="stylesheet" type="text/css" href="{!! asset('css/rest-closeby.css') !!}"> --}}
 @endsection
 
 @section('content')
     @include ('partials.alerts')
     <div id="hero">
         <div class="hero-image"></div>
+        <div class='hero-overlay'></div>
         <div class='hero-content'>
             <h1 class='welcome-text'>The new way of dining</h1>
-            
-            <form action="{{ route('searchsite') }}">
-                <i class='fa fa-search'></i>
-                <input type="text" id='query' name="query"  required placeholder="Location or Restaurant" dir = "auto">
-                <select>
-                    @@foreach (range(1, 10) as $option)
-                        <option value="{{$option}}">{{$option}} seat</option>
-                    @endforeach
-                </select>
+            <h5>Search &amp; book your restaurant table</h5>
+            <form action="{{ route('searchsite') }}" class=''>
+                <div class="row">
+                    <div class="col col-sm-5 col-lg-8 col-md-8">
+                        <div class="input-group">
+                            <span class='input-group-addon'><i class='fa fa-search'></i></span>
+                            <input type="text" class="form-control" name="query" placeholder="Search for restaurant... name or location">
+                        </div>
+                    </div>
 
-                <button type="submit" id='search-button' class="btn btn-default" >Search for restaurant closest to you</button>
+                    <div class="col col-sm-2 col-md-2 col-lg-2">
+                        <div class='input-group'>
+                            <select class='form-control'>
+                                @foreach (range(1, 10) as $option)
+                                    <option value="{{$option}}">{{$option}} seat</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class='col col-sm-2 col-md-2 col-lg-2'>
+                        <button class="btn btn-default" type="submit">Go!</button>
+                    </div>
+                </div>
             </form>
+
         </div>
+    </div>
+    <div class="belt">
+        <h5><i class='fa fa-glass'></i> Featured restaurants</h5>
+    </div>
+
+    <div class="container ">
+        <div class="row">
+            <div id='featured-restaurant-scroller' class='owl-carousel'>
+                @foreach($featuredRestaurants as $index=>$restaurant)
+                    <div class="restaurant-cell">
+                        <a href="#" >
+                            <img src="http://lorempixel.com/400/200/food/{{$index}}" class="thumbnail"/>
+                            <div class='restaurant-info'>
+                                <h5 class='restaurant-title'>
+                                    {{$restaurant->name}}
+                                    <span class='price pull-right'>
+                                        ${{number_format((rand(200, 2000)/10), 2)}}
+                                    </span>
+                                </h5>
+
+                                <div class="info">
+                                    {{$restaurant->description}}
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipisicing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua.
+                                    Ut enim ad minim veniam, quis nostrud
+                                    exercitation ullamco laboris nisi ut aliquip
+                                    ex ea commodo consequat. Duis aute irure dolor
+                                    in reprehenderit in voluptate velit esse cillum
+                                    dolore eu fugiat nulla pariatur. Excepteur
+                                    sint occaecat cupidatat non proident, sunt
+                                    in culpa qui officia deserunt mollit anim
+                                    id est laborum.
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+
+    </div>
+
+
+    <div class="belt">
+        <h5>Restaurants Closeby</h5>
+    </div>
+    <div class="container">
+        <div class="row" id="closeby-restaurants">
+            <h5>
+                <i class="fa fa-spinner fa-spin"></i>
+                Fetching restaurants close to you...
+            </h5>
+        </div>
+    </div>
+
+
+    <div class="belt">
+        <h5>Latest restaurants</h5>
+    </div>
+    <div id="latest-restaurants-section">
+        <div class='container'>
+            <div class='row'>
+                @foreach($latestRestaurants as $restaurant)
+                    <div class='col col-md-4'>
+                        <ul class='latest-restaurant-items'>
+                            <li>
+                                <span class='title'>
+                                    <a href="#">{{$restaurant->name}}</a>
+                                    <strong class="pull-right price">${{number_format((rand(200, 2000)/10), 2)}}</strong>
+                                </span>
+                                <p class='info short'>
+                                    {{$restaurant->description}} Si ut domesticarum et e fugiat instituendarum o senserit an cernantur an noster
+                                    et arbitror ita lorem, vidisse varias proident quamquam ita nescius non
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+                @endforeach
+
+            </div>
+        </div>
+        <h5 class="home-footer">#TeamResly</h5>
     </div>
 @endsection
 @section('scripts')
   <script type='text/javascript' src="{!! asset('owl-carousel/owl.carousel.min.js') !!}"></script>
-  <script type='text/javascript' src="{!! asset('js/ellipsis.js') !!}"></script>
+  {{-- <script type='text/javascript' src="{!! asset('js/ellipsis.js') !!}"></script> --}}
   <script type='text/javascript' src="{!! asset('js/welcome.js') !!}"></script>
   <script type="text/javascript" src="{!! asset('js/search_form_verification.js') !!}"></script>
   <script type="text/javascript" src="{!! asset('js/geolocation.js') !!}"></script>
