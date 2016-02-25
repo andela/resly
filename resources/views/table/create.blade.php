af20435684443226720dc411981ce344de9895bf
andela-badebiyi <bodunde.adebiyi@andela.com>
2016-02-25 12:31:01 -0800

    [Feature #114380111] Add Avatar to to restaurant tables

--- resources/views/table/create.blade.php
+++ resources/views/table/create.blade.php
@@ -1,8 +1,9 @@
-@extends('dashboard.index')
+@extends('dashboard.restaurant')
 
 @section('title', 'Add Tables')
 
 @section('details')
+    @parent
     <div class="content">
         <div class="col-sm-6">
             @if(Session::has('success'))
@@ -12,7 +13,7 @@
             @endif
 
             <h4>Create new Table</h4>
-            <form method="post" action="{{url('/tables/')}}">
+            <form method="post" action="{{url('/tables/')}}" enctype="multipart/form-data">
                 <input type="hidden" id="rest_id" value="{{ $restaurant_id }}" name="restaurant_id">
                 @include('table.partials._add_form')
                 <button type = "submit" class="btn btn-primary" id="add_table">Add</button>
