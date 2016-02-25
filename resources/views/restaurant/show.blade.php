af1b9633d69ef456d9ce5bec297cb892c6c0816f
andela-gjames <george.james@andela.com>
2016-02-25 16:58:53 +0000

    [Feature] Redesigned dashboard

--- resources/views/restaurant/show.blade.php
+++ resources/views/restaurant/show.blade.php
@@ -20,6 +20,43 @@
                         <div class='not-available-label'>
                             <h3 class="text-muted text-center">You have no tables in this restaurant</h3>
                         </div>
+                    @else
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <td colspan="5"><h4 class="text-center">Seats in restaurant</h4></td>
+                                </tr>
+                                <tr>
+                                    <td>SN</td>
+                                    <td>Name</td>
+                                    <td>Number of seats</td>
+                                    <td>Cost</td>
+                                    <td class='status'>Bookings</td>
+                                    <td>Action</td>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($tables as $index=>$table)
+                                    <tr>
+                                        <td>{{$index+1}}</td>
+                                        <td>{{$table->label}}</td>
+                                        <td>{{$table->seats_number}}</td>
+                                        <td>$ {{number_format($table->cost, 2)}}</td>
+                                        <td class='status booked'>10</td>
+                                        <td>
+                                            <a href="{{url("/tables/$table->id/edit/")}}" title="Edit">
+                                                <i class="fa fa-pencil"></i>
+                                            </a>
+                                            {!! Form::open(['url'=>url("/tables/$table->id/delete/"),
+                                             'method'=>'delete','class'=>'inline-form delete-form']) !!}
+                                                <button type="submit" name="delete"><i class="fa fa-trash"></i></button>
+                                            {!! Form::close()!!}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     @endif
                 </div>
             </div>
